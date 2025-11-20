ca13e705abdd961af1090eef427fb21acab1f691
Malimar555132 <jamesoconnor132@gmail.com>
2025-11-20 01:48:23 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "dino2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c c c . . . . 
+. . . . . . c c 7 7 7 7 c c . . 
+. . . . . c 7 7 7 7 7 7 7 7 c . 
+. . . . c 7 7 7 f 1 7 7 7 7 7 c 
+. . . c 7 7 7 7 f f 7 7 7 7 7 c 
+. . . c 7 7 7 7 7 7 7 7 7 7 7 c 
+. . c 6 7 7 7 7 7 7 b 1 b b c c 
+. . c 6 6 6 7 7 7 7 7 3 3 3 7 c 
+. . c 6 6 6 7 7 7 7 7 7 7 7 b . 
+. c c 6 6 6 6 b 7 7 c b b c . . 
+c 6 c 6 6 6 6 b b 7 7 c b b c . 
+c 6 6 6 6 6 6 6 6 c c c c c c . 
+. c 6 6 6 b 7 7 6 c c c c . . . 
+. . c c c b 7 7 b c c c c c . . 
+. . . . c b 7 7 6 c b b b c . . 
+`;
+            case "image1":
+            case "dino1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c c c . . . . 
+. . . . . . c c 5 5 5 5 c c . . 
+. . . . . c 5 5 5 5 5 5 5 5 c . 
+. . . . c 5 5 5 f 1 5 5 5 5 5 c 
+. . . c 5 5 5 5 f f 5 5 5 5 5 c 
+. . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+. . c d 5 5 5 5 5 5 b 1 b b c c 
+. . c d d d 5 5 5 5 5 3 3 3 5 c 
+. . c d d d 5 5 5 5 5 5 5 5 b . 
+. c c d d d d b 5 5 c b b c . . 
+c d c d d d d b b 5 5 c b b c . 
+c d d d d d d d d c c c c c c . 
+. c d d d b 5 5 d c c c c . . . 
+. . c c c b 5 5 b c c c c c . . 
+. . . . c b 5 5 d c b b b c . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
